c81b7021e7b2b812cd04935118a0a333f69f2cba
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-04 17:05:42 -0800

    Add student cpt and taxonomy

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -47,6 +47,55 @@ function hk_register_custom_post_types() {
         'supports'           => array( 'title' ),
     );
     register_post_type( 'hk-staff', $args );
+
+    // Register Students
+    $labels = array(
+        'name'                  => _x( 'Students', 'post type general name' ),
+        'singular_name'         => _x( 'Student', 'post type singular name'),
+        'menu_name'             => _x( 'Students', 'admin menu' ),
+        'name_admin_bar'        => _x( 'Student', 'add new on admin bar' ),
+        'add_new'               => _x( 'Add New', 'Student' ),
+        'add_new_item'          => __( 'Add New Student' ),
+        'new_item'              => __( 'New Student' ),
+        'edit_item'             => __( 'Edit Student' ),
+        'view_item'             => __( 'View Student' ),
+        'all_items'             => __( 'All Students' ),
+        'search_items'          => __( 'Search Students' ),
+        'parent_item_colon'     => __( 'Parent Students:' ),
+        'not_found'             => __( 'No Students found.' ),
+        'not_found_in_trash'    => __( 'No Students found in Trash.' ),
+        'archives'              => __( 'Student Archives'),
+        'insert_into_item'      => __( 'Insert into Student'),
+        'uploaded_to_this_item' => __( 'Uploaded to this Student'),
+        'filter_item_list'      => __( 'Filter Students list'),
+        'items_list_navigation' => __( 'Students list navigation'),
+        'items_list'            => __( 'Students list'),
+        'featured_image'        => __( 'Student featured image'),
+        'set_featured_image'    => __( 'Set Student featured image'),
+        'remove_featured_image' => __( 'Remove Student featured image'),
+        'use_featured_image'    => __( 'Use as featured image'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_nav_menus'  => true,
+        'show_in_admin_bar'  => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'students' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 6,
+        'menu_icon'          => 'dashicons-id',
+        'supports'           => array( 'thumbnail' ),
+    );
+
+    register_post_type( 'hk-student', $args );
 }
 add_action( 'init', 'hk_register_custom_post_types' );
 
@@ -80,6 +129,34 @@ function hk_register_taxonomies() {
         'rewrite'           => array( 'slug' => 'staff-categories' ),
     );
     register_taxonomy( 'hk-staff-category', array( 'hk-staff' ), $args );
+
+    // Add Student Category taxonomy
+    $labels = array(
+        'name'              => _x( 'Student Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Student Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Student Categories' ),
+        'all_items'         => __( 'All Student Category' ),
+        'parent_item'       => __( 'Parent Student Category' ),
+        'parent_item_colon' => __( 'Parent Student Category:' ),
+        'edit_item'         => __( 'Edit Student Category' ),
+        'view_item'         => __( 'View Student Category' ),
+        'update_item'       => __( 'Update Student Category' ),
+        'add_new_item'      => __( 'Add New Student Category' ),
+        'new_item_name'     => __( 'New Student Category Name' ),
+        'menu_name'         => __( 'Student Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'student-categories' ),
+    );
+    register_taxonomy( 'hk-student-category', array( 'hk-student' ), $args );
 }
 add_action( 'init', 'hk_register_taxonomies');
 ?>
