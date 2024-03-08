c5700702a1526303c0958b0ff4d4115f7f249b7c
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-07 20:13:39 -0800

    Add and update footer styles

--- footer.php
+++ footer.php
@@ -13,18 +13,22 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hk-school' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hk-school' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hk-school' ), 'hk-school', '<a href="https://kevanngan.com/school">Kevan Ngan, Hojin Chang</a>' );
-				?>
+			<nav><?php echo wp_get_attachment_image( 100, 'full'); ?></nav>
+
+			<section class="footer-credits">
+				<h2>Credits</h2>
+				<p><?php printf( esc_html__( 'Created by %s' ), '<a href="https://kevanngan.com/school">Kevan Ngan & Hojin Chang</a>' ); ?></p>
+				<p>Photos courtesy of <a href="https://www.shopify.com/stock-photos">Burst.</a></p>
+			</section>
+
+			<nav class="footer-nav">
+				<h2>Links</h2>
+				<ul id="menu-footer-navigation" class="menu"></ul>
+			</nav>
+
 		</div><!-- .site-info -->
+
+		<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
