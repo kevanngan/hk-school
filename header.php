dc6e188553030c23c2e5a1308ec5d5a34168f312
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-03-07 18:42:04 -0800

    Change header and links colours

--- header.php
+++ header.php
@@ -27,17 +27,8 @@
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
 			$hk_school_description = get_bloginfo( 'description', 'display' );
 			if ( $hk_school_description || is_customize_preview() ) :
 				?>
