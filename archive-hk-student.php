d4c1e9624092d99757961e54a67943d499418f3c
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-12 18:41:12 -0700

    Update format according to screen widths

--- archive-hk-student.php
+++ archive-hk-student.php
@@ -17,45 +17,46 @@ get_header();
 			<h1><?php single_term_title(); ?></h1>
 		</header><!-- .page-header -->
 
-		<?php 
-		$args = array(
-			'post-type' 	   => 'hk-student',
-			'posts_per_page'   => -1,
-			'orderby'		   => 'title',
-			'order'            => 'ASC',
-			'tax_query'	       => array(
-				array(
-					'taxonomy' => 'hk-student-category',
-					'field'	   => 'slug',
-					'terms'    => array( 'designer', 'developer,' ),
-					'operator' => 'IN',
+		<section class="students">
+			<?php 
+			$args = array(
+				'post-type' 	   => 'hk-student',
+				'posts_per_page'   => -1,
+				'orderby'		   => 'title',
+				'order'            => 'ASC',
+				'tax_query'	       => array(
+					array(
+						'taxonomy' => 'hk-student-category',
+						'field'	   => 'slug',
+						'terms'    => array( 'designer', 'developer,' ),
+						'operator' => 'IN',
+					),
 				),
-			),
-		);
-
-		$query = new WP_Query( $args );
-		if ( $query -> have_posts()) {
-			while ( $query -> have_posts() ) {
-				$query -> the_post();
-				?>
-				<article class="student-item">
-				<h2><a href="<?php the_permalink(); ?>" class="main-link"><?php the_title(); ?></a></h2>
-				<a href="<?php the_permalink(); ?>" class="main-link">
-					<?php the_post_thumbnail('student-thumbnail-300x257'); ?>
-				</a>
-				<p>
-					<?php 
-						echo wp_trim_words(get_the_excerpt(), 25, ''); 
-						echo '<br><a href="' . get_permalink() . '" class="main-link">Read more about the student...</a>';
+			);
+			$query = new WP_Query( $args );
+			if ( $query -> have_posts()) {
+				while ( $query -> have_posts() ) {
+					$query -> the_post();
 					?>
-				</p>
-				<p><?php echo get_the_term_list(get_the_ID(), 'hk-student-category', 'Specialty: '); ?></p>
-			</article>
-				<?php
+					<article class="student-item">
+					<h2><a href="<?php the_permalink(); ?>" class="main-link"><?php the_title(); ?></a></h2>
+					<a href="<?php the_permalink(); ?>" class="main-link">
+						<?php the_post_thumbnail('student-thumbnail-300x257'); ?>
+					</a>
+					<p>
+						<?php 
+							echo wp_trim_words(get_the_excerpt(), 25, ''); 
+							echo '<br><a href="' . get_permalink() . '" class="main-link">Read more about the student...</a>';
+						?>
+					</p>
+					<p><?php echo get_the_term_list(get_the_ID(), 'hk-student-category', 'Specialty: '); ?></p>
+					</article>
+					<?php
+				}
+				wp_reset_postdata();
 			}
-			wp_reset_postdata();
-		}
-		?>
+			?>
+		</section>
 
 
 	</main><!-- #main -->
