a443fc62df17eae1ca71389ade3f473fc5916080
Kevan Ngan <kevanngan_@hotmail.com>
2024-03-18 17:18:27 -0700

    Fix typo for button class

--- single-hk-student.php
+++ single-hk-student.php
@@ -59,7 +59,7 @@ get_header();
 				if ( $query->have_posts() ) {
 					while ( $query->have_posts() ) {
 						$query->the_post();
-						echo '<p><a href="' . esc_url( get_permalink() ) . '" class="main-link under-lin">' . get_the_title() . '</a></p>';
+						echo '<p><a href="' . esc_url( get_permalink() ) . '" class="main-link underline-link">' . get_the_title() . '</a></p>';
 					}
 					wp_reset_postdata();
 				}
